c34acef1ef0dce7a4c48a9985c06e15ab93a7c5d
Sina <sina@noobesang@gmail.com>
2022-05-25 13:51:59 -0400

    bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,19 +7,18 @@ import { ApolloProvider } from '@apollo/client'
 import client from '../apollo-client'
 import { Toaster } from 'react-hot-toast'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
- return (
-  <ApolloProvider client={client}>
-   <SessionProvider session={session}>
-    <Toaster />
-    <div className="h-screen overflow-y-scroll bg-slate-200">
-     <Header />
-     <Component {...pageProps} />
-    </div>
-   </SessionProvider>
-  </ApolloProvider>
- )
+function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
+  return (
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <Toaster />
+        <div className="h-screen overflow-y-scroll bg-slate-200">
+          <Header />
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
-
